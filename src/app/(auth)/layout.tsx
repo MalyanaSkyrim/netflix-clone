0d8b6a52745d26f8ff8d1d91bea3aedd29b41d4d
Mohamed <Malyana>
2024-09-08 17:35:31 +0100

    init project structure

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React, { ReactNode } from "react";
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src="/login_background.jpg"
+        alt="image"
+        className="hidden sm:block sm:object-cover -z-10 brightness-50"
+        fill
+        priority
+      />
+      <Image
+        src="/netflix_logo.svg"
+        alt="image"
+        priority
+        className="absolute left-4 top-4 object-contain md:left-10 md:top-6"
+        width={120}
+        height={120}
+      />
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
